98b98c43b23c80e36d62acf9f79a0f6cd2b3dede
Temitope Joseph <126495757+Tjoseph-O@users.noreply.github.com>
2023-04-24 21:27:25 +0100

    Create odd_even_number.py

---
+++ Day3/odd_even_number.py
@@ -0,0 +1,7 @@
+number = int(input("Which number do you want to check? "))
+take_in_number = number % 2
+
+if take_in_number == 0:
+    print("This is an Even Number")
+else:
+    print("This is an Odd Number")
